cc47de82617157a294c86e269d08171cdd076722
suyeon <rlatndus9911@naver.com>
2023-07-17 22:01:32 +0900

    로그인 페이지 추가

---
+++ src/App3.js
@@ -0,0 +1,15 @@
+import loginpage from './image/loginpage.png'
+
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+        <div className="con">
+            <img className="loginpage" src={loginpage}/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
